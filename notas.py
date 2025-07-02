caaa7a16f08fd0b8eb8f286ed90e3a38e316be72
Arnhe <arteagaarne@gmail.com>
2025-07-02 11:35:03 -0400

    Actualizar notas.py

--- notas.py
+++ notas.py
@@ -1,27 +1,25 @@
 materias_y_notas = [
-    ["DEFENSA INTEGRAL DE LA NACIÓN I", 17],
-    ["DIBUJO", 17],
-    ["EDUCACIÓN AMBIENTAL", 20],
+    ["DEFENSA INTEGRAL DE LA NACIÓN I", 17], 
+    ["DIBUJO", 17], 
+    ["EDUCACIÓN AMBIENTAL", 20], 
     ["GEOMETRÍA ANALÍTICA", 16],
-    ["HOMBRE, SOCIEDAD, CIENCIA Y TECNOLOGÍA", 19],
-    ["INGLÉS I", 17],
-    ["MATEMÁTICA I", 20],
-    ["SEMINARIO I", 18],
-    ["FÍSICA I", 14],
-    ["QUÍMICA GENERAL", 14],
-
-    ["PROBABILIDADES Y ESTADÍSTICA", 11],
-    ["ÁLGEBRA LINEAL", 13],
-    ["DEFENSA INTEGRAL DE LA NACIÓN II", 16],
-    ["INGLÉS II", 15],
-    ["MATEMÁTICA II", 16],
-    ["SEMINARIO II", 14],
-    ["EDUCACIÓN FÍSICA Y DEPORTE", 18],
-    ["FÍSICA II", 10],
-
-    ["MATEMÁTICA III", 16],
+    ["HOMBRE, SOCIEDAD, CIENCIA, Y TECNOLOGIA", 19], 
+    ["INGLÉS I", 17], 
+    ["MATEMÁTICA I", 20], 
+    ["SEMINARIO I", 18], 
+    ["FISICA I", 14], 
+    ["QUÍMICA GENERAL", 14], 
+    ["PROBABILIDADES Y ESTADÍSTICAS", 11], 
+    ["ALGEBRA LINEAL", 13], 
+    ["DEFENSA INTEGRAL DE LA NACIÓN II", 16], 
+    ["INGLES II",15], 
+    ["MATEMÁTICA II", 16], 
+    ["SEMINARIO II", 14], 
+    ["EDUCACIÓN FÍSICA Y DEPORTE", 18], 
+    ["FISICA II",10], 
+    ["MATEMATICA III", 16], 
 ]
-
+    
 print("--- Tus Materias y Notas ---")
 for materia, nota in materias_y_notas:
     print(f"Materia: {materia}, Nota: {nota}")
@@ -33,5 +31,8 @@ if materias_y_notas:
     print(f"\nTu promedio general es: {promedio:.2f}")
 else:
     print("\nNo hay materias registradas.")
+
+
+
 
 
